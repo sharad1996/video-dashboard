b2b4fc6cee9a35302217a19015cbb8e4fe13e6ab
Sharad <sharadwankhade23@gmail.com>
2020-11-13 19:33:45 +0530

    Home page Analytics Graph functionality

---
+++ src/components/activity/activity.component.js
@@ -0,0 +1,93 @@
+import React from 'react'
+import moment from 'moment';
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import './activity.styles.scss';
+
+const useStyles = makeStyles({
+    formControl: {
+        minWidth: 200,
+        maxHeight: 50,
+    },
+    formControlBuilding: {
+        minWidth: '100%',
+        maxHeight: 50,
+    }
+});
+
+export default function Activity(props) {
+
+    const classes = useStyles();
+    const {playVideo, age, handleChange, videos, activeIdVideo} = props
+    return (
+        <div className='location-block'>
+            <div className='p-3'>
+                <div className='d-flex justify-content-between px-3'>
+                    <div className=''>Location</div>
+                    <div className='filter'>Filters+</div>
+                </div>
+                <div className='d-flex justify-content-between'>
+                    <FormControl variant="outlined" className={classes.formControlBuilding}>
+                        <InputLabel id="demo-simple-select-outlined-label">Building Name</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-outlined-label"
+                            id="demo-simple-select-outlined"
+                            value={age}
+                            onChange={()=>handleChange()}
+                            label="Building Name"
+                        >
+                            <MenuItem value="">
+                                <em>None</em>
+                            </MenuItem>
+                            <MenuItem value={10}>Ten</MenuItem>
+                            <MenuItem value={20}>Twenty</MenuItem>
+                            <MenuItem value={30}>Thirty</MenuItem>
+                        </Select>
+                    </FormControl>
+                </div>
+                <hr />
+                <div className='d-flex justify-content-between align-items-center'>
+                    <div className='d-block'>
+                        <FormControl variant="outlined" className={classes.formControl}>
+                            <InputLabel id="demo-simple-select-outlined-label">Age All</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-outlined-label"
+                                id="demo-simple-select-outlined"
+                                value={age}
+                                onChange={handleChange}
+                                label="Age All"
+                            >
+                                <MenuItem value="">
+                                    <em>None</em>
+                                </MenuItem>
+                                <MenuItem value={10}>Ten</MenuItem>
+                                <MenuItem value={20}>Twenty</MenuItem>
+                                <MenuItem value={30}>Thirty</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </div>
+                    <div className='filter pr-3'>Filters+</div>
+                </div>
+            </div>
+            <div>
+                {videos.map((data) => {
+                    return (
+                        <div className={`px-4 d-flex align-items-center mb-4 ${activeIdVideo === data.id && 'active-video'}`} key={data.id} onClick={() => playVideo(this, data.id)}>
+                            <img src={data.image} alt={data.heading}/>
+                            <div className='d-block pl-3 pr-5'>
+                                <div><strong>{data.heading}</strong></div>
+                                <div>{data.description}</div>
+                            </div>
+                            <div className='px-4'>
+                                <div>{moment(data.time).format('h:mm a')}</div>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    )
+}
--- src/components/home/home.component.js
+++ src/components/home/home.component.js
@@ -4,12 +4,10 @@ import Paper from '@material-ui/core/Paper';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import './home.styles.scss';
-import moment from 'moment';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
 import { Player } from 'video-react';
+// import DemoAppArea from './demo-app-area';
+import Activity from '../activity/activity.component';
+import Analytics from '../analytics/analytics.component';
 import DemoAppArea from './demo-app-area.component';
 
 const useStyles = makeStyles({
@@ -33,7 +31,10 @@ function Home() {
 	const [activeIdVideo, setActiveIdVideo] = React.useState(0);
     const classes = useStyles();
     const [age, setAge] = React.useState('');
+    const [sidePanelValue, setSidePanelValue] = React.useState('Activity')
     const [video, setVideo] = React.useState('');
+
+    console.log(sidePanelValue)
 	const tags = [
 		{id: 0, name: 'Tenant', link: 'https://www.youtube.com/watch?v=C-hgCVxKOGE'},
 		{id: 1, name: 'Animal', link: 'https://www.youtube.com/watch?v=C-hgCVxKOGE'},
@@ -64,6 +65,10 @@ function Home() {
     const handleClick = (ev, data) => {
         setActiveIdType(data.id);
         setVideo(data.link);
+    }
+
+    const handleSideTabClick = (data) => {
+        setSidePanelValue(data);
     }
     
     const playVideo = (ev, id) => {
@@ -118,76 +123,16 @@ function Home() {
                         textColor="primary"
                         aria-label="icon tabs example"
                     >
-                        <Tab icon={'Activity'} aria-label="phone" />
-                        <Tab icon={'Analytics'} aria-label="favorite" />
+                        <Tab icon={'Activity'} aria-label="phone" onClick={() => handleSideTabClick('Activity')} />
+                        <Tab icon={'Analytics'} aria-label="favorite" onClick={() => handleSideTabClick('Analytics')}/>
                     </Tabs>
                 </Paper>
-                <div className='location-block'>
-                    <div className='p-3'>
-                        <div className='d-flex justify-content-between px-3'>
-                            <div className=''>Location</div>
-                            <div className='filter'>Filters+</div>
-                        </div>
-                        <div className='d-flex justify-content-between'>
-                            <FormControl variant="outlined" className={classes.formControlBuilding}>
-                                <InputLabel id="demo-simple-select-outlined-label">Building Name</InputLabel>
-                                <Select
-                                    labelId="demo-simple-select-outlined-label"
-                                    id="demo-simple-select-outlined"
-                                    value={age}
-                                    onChange={handleChange}
-                                    label="Building Name"
-                                >
-                                    <MenuItem value="">
-                                        <em>None</em>
-                                    </MenuItem>
-                                    <MenuItem value={10}>Ten</MenuItem>
-                                    <MenuItem value={20}>Twenty</MenuItem>
-                                    <MenuItem value={30}>Thirty</MenuItem>
-                                </Select>
-                            </FormControl>
-                        </div>
-                        <hr />
-                        <div className='d-flex justify-content-between align-items-center'>
-                            <div className='d-block'>
-                                <FormControl variant="outlined" className={classes.formControl}>
-                                    <InputLabel id="demo-simple-select-outlined-label">Age All</InputLabel>
-                                    <Select
-                                        labelId="demo-simple-select-outlined-label"
-                                        id="demo-simple-select-outlined"
-                                        value={age}
-                                        onChange={handleChange}
-                                        label="Age All"
-                                    >
-                                        <MenuItem value="">
-                                            <em>None</em>
-                                        </MenuItem>
-                                        <MenuItem value={10}>Ten</MenuItem>
-                                        <MenuItem value={20}>Twenty</MenuItem>
-                                        <MenuItem value={30}>Thirty</MenuItem>
-                                    </Select>
-                                </FormControl>
-                            </div>
-                            <div className='filter pr-3'>Filters+</div>
-                        </div>
-                    </div>
-                    <div>
-                        {videos.map((data) => {
-                            return (
-                                <div className={`px-4 d-flex align-items-center mb-4 ${activeIdVideo === data.id && 'active-video'}`} key={data.id} onClick={() => playVideo(this, data.id)}>
-                                    <img src={data.image} alt={data.heading}/>
-                                    <div className='d-block pl-3 pr-5'>
-                                        <div><strong>{data.heading}</strong></div>
-                                        <div>{data.description}</div>
-                                    </div>
-                                    <div className='px-4'>
-                                        <div>{moment(data.time).format('h:mm a')}</div>
-                                    </div>
-                                </div>
-                            );
-                        })}
-                    </div>
-                </div>
+                {sidePanelValue=='Activity'?
+                    <Activity playVideo={playVideo} age={age} handleChange={handleChange} videos={videos} activeIdVideo={activeIdVideo}/>
+                :
+                <Analytics/>
+                }
+                
             </div>
         </div>
 	);
